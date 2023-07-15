6b02aafb69be1df0f1cae1225456c7b2be10bb64
Megan Warres <megan.warres@gmail.com>
2023-07-14 18:18:58 -0700

    feat(carousel): Automatically advance photos
    Add a timeout to automatically advance photos every 5 seconds. The 5
    seconds resets when the user uses a button to view the next or previous
    photo.

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Carousel({ images }: { images: React.JSX.Element[] }) {
   const [currImage, setCurrImage] = useState(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout>();
 
   const viewPrevImage = () => {
+    clearInterval(intervalId);
     setCurrImage((currImage + images.length - 1) % images.length);
   };
 
   const viewNextImage = () => {
+    clearInterval(intervalId);
     setCurrImage((currImage + 1) % images.length);
   };
+
+  useEffect(() => {
+    setIntervalId(
+      setInterval(() => {
+        setCurrImage((currImage + 1) % images.length);
+      }, 5000),
+    );
+    return () => clearInterval(intervalId);
+  }, [currImage]);
 
   return (
     <div className="carousel-container">
